847d840051918a1d7c3e0c480e67f44b1d9c52cd
QuTianbai <tianbai@qutianbaideMacBook-Pro.local>
2016-06-05 23:10:44 +0800

    tianbai

---
+++ GetPayURLScheme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetPayURLScheme
+//
+//  Created by 曲天白 on 16/6/5.
+//  Copyright © 2016年 曲天白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetPayURLScheme/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetPayURLScheme
+//
+//  Created by 曲天白 on 16/6/5.
+//  Copyright © 2016年 曲天白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
